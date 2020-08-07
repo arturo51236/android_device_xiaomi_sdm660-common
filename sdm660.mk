2ab1a78decd240c5fc25b20ab0fcb517b4d2e831
arturo51236 <rtsanonynousperson@gmail.com>
2020-08-07 04:26:08 +0200

    Nuke unnecessary PixelWallpapers

--- sdm660.mk
+++ sdm660.mk
@@ -599,10 +599,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
-# Wallpapers
-PRODUCT_PACKAGES += \
-    PixelLiveWallpaperPrebuilt
-
 # XiaomiParts
 PRODUCT_PACKAGES += \
    XiaomiParts
